81ffbb6558908d34c22eb6f1341cbdb0ac946ed6
Ravan <ravan2003@gmail.com>
2025-03-17 21:35:51 +0800

    Adjusted card width to make it bigger

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card({ text, response, onDelete }) {
   return (
-    <div className="p-3 border border-gray-200 bg-white rounded-lg w-[25ch] flex items-start gap-2 mb-3 mx-2 max-md:w-[80vw]">
+    <div className="p-3 border border-gray-200 bg-white rounded-lg w-[30ch] flex items-start gap-2 mb-3 mx-2 max-md:w-[80vw]">
       <div className="flex flex-col">
         <div className="w-full break-words overflow-hidden font-bold">
           {text}
